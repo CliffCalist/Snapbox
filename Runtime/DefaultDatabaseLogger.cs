6c2e141264d668980fbc62f7361c0358f1721cf7
Cliff Calist <cliffstersprog@gmail.com>
2025-03-09 19:05:27 +0100

    thread-safe DefaultDatabaseLogger

---
+++ Runtime/DefaultDatabaseLogger.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhiteArrow.DataSaving
+{
+    public class DefaultDatabaseLogger : MonoBehaviour, IDatabaseLogger
+    {
+        private readonly object _logLock = new();
+        private readonly Queue<string> _logMessages = new();
+
+        private readonly object _errorLock = new();
+        private readonly Queue<string> _errorMessages = new();
+
+
+
+        private void Update()
+        {
+            lock (_logLock)
+            {
+                while (_logMessages.Count > 0)
+                {
+                    var message = _logMessages.Dequeue();
+                    Debug.Log(message);
+                }
+            }
+
+            lock (_errorLock)
+            {
+                while (_errorMessages.Count > 0)
+                {
+                    var message = _errorMessages.Dequeue();
+                    Debug.LogError(message);
+                }
+            }
+        }
+
+
+
+        public void Log(string message)
+        {
+            lock (_logLock)
+            {
+                _logMessages.Enqueue(message);
+            }
+        }
+
+        public void LogError(string message)
+        {
+            lock (_errorLock)
+            {
+                _errorMessages.Enqueue(message);
+            }
+        }
+    }
+}
