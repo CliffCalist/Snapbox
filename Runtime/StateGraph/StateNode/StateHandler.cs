48da97f1e34832bd3029c2fabc8b69a06351807e
Cliff Calist <cliffstersprog@gmail.com>
2025-05-02 01:00:01 +0200

    refactor(state-graph): make StateHandler inherit from StateNode to enable scene-wide node search

--- Runtime/StateGraph/StateNode/StateHandler.cs
+++ Runtime/StateGraph/StateNode/StateHandler.cs
@@ -1,42 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace WhiteArrow.SnapboxSDK
 {
-    public abstract class StateHandler : MonoBehaviour, IStateHandler
+    public abstract class StateHandler : StateNode, IStateHandler
     {
-        private StateGraphContext _context;
-
-
-
-        public void SetContext(StateGraphContext context)
-        {
-            _context = context ?? throw new System.ArgumentNullException(nameof(context));
-        }
-
-
-
-        private void Awake()
-        {
-            if (_context == null)
-                throw new System.Exception("The context is not set.");
-
-            if (_context.Phase == StateGraphPhase.Capturing)
-                InitEntity();
-        }
-
-        public virtual void InitEntity() { }
-
-
-
-        public virtual IEnumerable<IStateNode> GetChildren()
-        {
-            return Enumerable.Empty<IStateNode>();
-        }
-
-
-
         public abstract void RegisterSnapshotMetadata(Snapbox snapbox);
         public abstract void RestoreState(Snapbox snapbox);
         public abstract void CaptureState(Snapbox snapbox);
@@ -44,11 +11,11 @@ namespace WhiteArrow.SnapboxSDK
 
 
 
-    public abstract class StateHandler<T> : StateHandler
+    public abstract class StateHandler<T> : StateNode<T>
         where T : Component
     {
-        private T _cached;
-
-        public T Target => _cached ??= GetComponent<T>();
+        public abstract void RegisterSnapshotMetadata(Snapbox snapbox);
+        public abstract void RestoreState(Snapbox snapbox);
+        public abstract void CaptureState(Snapbox snapbox);
     }
 }
