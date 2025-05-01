108b22d1679e9cde4a8aeffff911aed46de4aef3
Cliff Calist <cliffstersprog@gmail.com>
2025-05-01 22:09:18 +0200

    feat(state-graph-context): changed 'Phase' access modifier to public

--- Runtime/StateGraph/StateGraphContext.cs
+++ Runtime/StateGraph/StateGraphContext.cs
@@ -5,13 +5,6 @@ namespace WhiteArrow.SnapboxSDK
     [CreateAssetMenu(fileName = "StateGraphContext", menuName = "Snapbox/StateGraphContext")]
     public class StateGraphContext : ScriptableObject
     {
-        [SerializeField] private StateGraphPhase _phase;
-
-
-        public StateGraphPhase Phase
-        {
-            get => _phase;
-            internal set => _phase = value;
-        }
+        public StateGraphPhase Phase;
     }
 }
