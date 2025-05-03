2338ef5ac6fcc430e31d1dae18961f574dc69565
Cliff Calist <cliffstersprog@gmail.com>
2025-05-03 15:12:12 +0200

    feat(state-node-editor): show derived fields in StateNode inspector

--- Editor/StateNodeEditor.cs
+++ Editor/StateNodeEditor.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using WhiteArrow.SnapboxSDK;
@@ -7,18 +8,47 @@ namespace WhiteArrowEditor.SnapboxSDK
     [CustomEditor(typeof(StateNode), true)]
     public class StateNodeEditor : Editor
     {
+        private const string INIT_INDEX_FIELD_NAME = "_initIndex";
+        private const string CHILDREN_FIELD_NAME = "_children";
+
+
+
+        private readonly string[] FIELD_NAMES_FOR_HIDE =
+        {
+            INIT_INDEX_FIELD_NAME,
+            CHILDREN_FIELD_NAME,
+            "m_Script",
+            "size"
+        };
+
+
+
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            var initIndexProp = serializedObject.FindProperty("_initIndex");
-            var childrenProp = serializedObject.FindProperty("_children");
+            var initIndexProp = serializedObject.FindProperty(INIT_INDEX_FIELD_NAME);
+            var childrenProp = serializedObject.FindProperty(CHILDREN_FIELD_NAME);
 
+            // Draw _initIndex as editable
             EditorGUILayout.PropertyField(initIndexProp);
 
+            // Draw _children as read-only
             GUI.enabled = false;
             EditorGUILayout.PropertyField(childrenProp, includeChildren: true);
             GUI.enabled = true;
+
+            // Draw all other visible serialized properties
+            SerializedProperty iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+            while (iterator.NextVisible(enterChildren))
+            {
+                if (FIELD_NAMES_FOR_HIDE.Contains(iterator.name))
+                    continue;
+
+                EditorGUILayout.PropertyField(iterator, includeChildren: true);
+                enterChildren = false;
+            }
 
             serializedObject.ApplyModifiedProperties();
         }
