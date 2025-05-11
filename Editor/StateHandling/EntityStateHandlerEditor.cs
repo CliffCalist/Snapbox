55c46ccd43ad16dada98259427f2012eeb70e8fd
Cliff Calist <cliffstersprog@gmail.com>
2025-05-11 18:10:01 +0200

    chore(entity-state-handler-editor): display _isRegistered, _isRestored and _isInitialized as read-only fields

--- Editor/StateHandling/EntityStateHandlerEditor.cs
+++ Editor/StateHandling/EntityStateHandlerEditor.cs
@@ -6,9 +6,15 @@ namespace WhiteArrowEditor.SnapboxSDK
     [CustomEditor(typeof(EntityStateHandler), true)]
     public class EntityStateHandlerEditor : Editor
     {
+        private static readonly string[] READ_ONLY_FIELD_NAMES =
+        {
+            "_isRegistered", "_isRestored", "_isInitialized"
+        };
+
+
         public override void OnInspectorGUI()
         {
-            EditorGuiUtility.DrawWithReadOnlyFields(serializedObject, "_isInitialized");
+            EditorGuiUtility.DrawWithReadOnlyFields(serializedObject, READ_ONLY_FIELD_NAMES);
         }
     }
 }
