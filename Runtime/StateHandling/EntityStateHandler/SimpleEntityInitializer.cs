de008cd17d0fa224a3fcb05d1279c43f56fbf387
Cliff Calist <cliffstersprog@gmail.com>
2025-07-18 14:29:21 +0200

    refactor(simple-entity-initializer): remove RequireComponent attribute

--- Runtime/StateHandling/EntityStateHandler/SimpleEntityInitializer.cs
+++ Runtime/StateHandling/EntityStateHandler/SimpleEntityInitializer.cs
@@ -1,8 +1,5 @@
-using UnityEngine;
-
 namespace WhiteArrow.SnapboxSDK
 {
-    [RequireComponent(typeof(ISimpleInitializable))]
     public class SimpleEntityInitializer : EntityStateHandler<ISimpleInitializable>
     {
         protected override void InitializeCore()
